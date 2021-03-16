6c79ffa2e3daf6204780a4863d7db1edc637b23f
wilfriedmuffins <wilfried.moffen@live.fr>
2021-03-16 08:36:19 +0100

    form ok, password maj pb flash_erors display resolve

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,14 @@ class SessionsController < ApplicationController
     def create
         #recupérer l'email l'user
         user = User.find_by(email: params[:email])
-
+            
         # .present? si la variable est no nil 
         if user.present? && user.authenticate(params[:password])
             session[:user_id]= user.id
             #flash[notice] = "bonjour vous étes connected?"
-
             redirect_to root_path, notice: "bonjour vous étes connecté deh!"
         else 
-            #flash[]
+            flash[:alert] = "email ou mot de passe invalide"
             render :new
         end    
         
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
   
-  get "password/reset", to: "password_resets#new"
-  post "password/reset", to: "password_resets#create"
+  #get "password/reset", to: "password_resets#new"
+  #post "password/reset", to: "password_resets#create"
 
 end
