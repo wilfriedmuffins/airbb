71462a164684efcec4e94b0a11bf1a1fd358da8d
wilfriedmuffins <wilfried.moffen@live.fr>
2021-03-18 08:07:43 +0100

    begining form the modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -35,9 +35,45 @@ document.addEventListener("turbolinks:load", () => {
     $(".experience-dropdown").on("click", function () {
         $(".input-voyageur").hide();
         $(".border-3").hide();
-    });
+    }),
+    
+    $('.link_modal').on("click", function(){
+      $(".modal").show();//toggleClass("show_modal_open");
+    }),
+
+    $('.close').on("click", function(){
+      $(".modal").hide();
+    })
+    ;
+
 })
 
+
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
 
 
 
