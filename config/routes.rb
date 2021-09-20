d29bb3f0484281c7d3eaa5b2629e95c8cc3d26ac
wilfriedmuffins <wilfried.moffen@live.fr>
2021-09-20 20:08:35 +0200

    delete user/password/password_reset/mailer/registration/session

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
 
-  get "users", to: "registrations#index"
-  get "sign_up", to: "registrations#new"
-  post "sign_up", to: "registrations#create"
-
-  get "sign_in", to: "pages#new"
-  post "sign_in", to: "pages#create" 
-  delete "logout", to: "pages#destroy"
-
   resources :logements
   get "search", to: "pages#search"
 
-  get "password", to: "passwords#edit", as: :edit_password
-  patch "password", to: "passwords#update"
+
   
-  get "password/reset", to: "password_resets#new"
-  post "password/reset", to: "password_resets#create"
-  get "password/reset/edit", to: "password_resets#edit"
-  patch "password/reset/edit", to: "password_resets#update"
 end
