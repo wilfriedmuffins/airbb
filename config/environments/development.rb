708f9de1346f84ceb5f33969a0c9e6bb58d03658
wilfriedmuffins <wilfried.moffen@live.fr>
2021-12-19 11:34:34 +0100

    rcarousel video on pages home done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,4 +77,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
   config.action_mailer.raise_delivery_errors = false
+  config.assets.check_precompiled_asset = false
 end
