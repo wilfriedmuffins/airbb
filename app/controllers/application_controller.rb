8730cf50b2fa6840997ad0dff70ca7fde70716a6
wilfriedmuffins <wilfried.moffen@live.fr>
2022-02-14 22:07:17 +0100

    add/ask avatar only for edit profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 
 end
