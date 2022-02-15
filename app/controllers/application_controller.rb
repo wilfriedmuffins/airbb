5e0dc4151fa503dccbac7d5b2d65f295e7720a65
wilfriedmuffins <wilfried.moffen@live.fr>
2022-02-15 20:51:41 +0100

    footer for booking show done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,11 +22,10 @@ class ApplicationController < ActionController::Base
         
     end
 
-
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :avatar_cache, :remove_avatar])
     end
 
 end
