35905316ae1dfe449cc4e8b90eb2c2b131e7da13
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-03 09:45:56 +0100

    debug with simple form without image and carouseul

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
     def home
         @latitude  = params[:latitude]
         @longitude  = params[:logitude]
-        puts @latitude.inspect
-        puts @longitude.inspect
+        # puts @latitude.inspect
+        # puts @longitude.inspect
     end
 
     def show
@@ -39,8 +39,8 @@ class PagesController < ApplicationController
             @voyageur = params[:voyageur]
             @latitude = params[:latitude]
             @longitude = params[:longitude]
-            puts @latitude.inspect
-            puts @longitude.inspect
+            # puts @latitude.inspect
+            # puts @longitude.inspect
             logement_non_disponible = Logement.joins(:bookings).where(["city = ? and start_date_of_availability <= ? and end_date_of_availability >= ? and logements.voyageur >= ? and bookings.start_booking <= ? and bookings.end_booking >= ?", @city, @arrive, @depart, @voyageur, @depart, @arrive])
             @logements_disponible = Logement.where(["city = ? and start_date_of_availability <= ? and end_date_of_availability >= ? and logements.voyageur >= ? and bookings_count = ?", @city, @arrive, @depart, @voyageur, 0]).or(Logement.where(["city = ? and start_date_of_availability <= ? and end_date_of_availability >= ? and logements.voyageur >= ? and bookings.start_booking > ?", @city, @arrive, @depart, @voyageur, @depart])).or(Logement.where(["city = ? and start_date_of_availability <= ? and end_date_of_availability >= ? and logements.voyageur >= ? and bookings.end_booking < ?", @city, @arrive, @depart, @voyageur, @arrive])).left_outer_joins(:bookings).distinct.without(logement_non_disponible)
         else
