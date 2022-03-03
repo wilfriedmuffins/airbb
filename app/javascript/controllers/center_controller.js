d5143743d7251ca5e80978f53f4ed4c95c64fab3
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-03 20:03:39 +0100

    add logoement_test.csv + add map

--- app/javascript/controllers/center_controller.js
+++ app/javascript/controllers/center_controller.js
@@ -1,33 +1,30 @@
 import { Controller } from "stimulus"
 export default class extends Controller {
-    static targets = [ "map"]//latitude", "longitude"]
+    static targets = [ "map", "latitude", "longitude"]
     static values = {
-      markers : Array}
-      // , 
-      // latitude : Number,
-      // longitude : Number}
+      markers : Array, 
+      latitude : Number,
+      longitude : Number}
 
     initialize() {
 
       console.log("in initialize");
 
-      console.log(markers.latitude,markers.latitude);
-
       this.map = new google.maps.Map(this.mapTarget, {
-        center: { lat: 48.856614 , lng: 2.3522219 },
-        zoom: 12,
+        center: { lat: this.latitudeValue, lng: this.longitudeValue },
+        zoom: 11,
       });
 
-      // this.markersValue.forEach((markers) => {
-      //   const marker = new google.maps.Marker({
-      //     position: { lat: markers.latitude, lng: markers.latitude },
-      //     map: this.map,
-      //     title: `${markers.title}`,
-      //     label: `${markers.price}€`,
-      //     optimized: false,
-      //   })
-      //  
-      // })
+      this.markersValue.forEach((markers) => {
+        const marker = new google.maps.Marker({
+          position: { lat: markers.latitude, lng: markers.longitude },
+          map: this.map,
+          title: `${markers.title}`,
+          label: `${markers.price}€`,
+          optimized: false,
+        })
+       
+      })
 
     }
 }
