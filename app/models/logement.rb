657a65057b66f9eebc67f59545bd4b339ef47f09
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-06 10:29:50 +0100

    add page san_francisco

--- app/models/logement.rb
+++ app/models/logement.rb
@@ -18,16 +18,16 @@ class Logement < ApplicationRecord
         adresse_changed? | city_changed? | zipcode_changed?
     end
 
-    belongs_to :user
-    has_many :bookings, dependent: :destroy
-    has_many :comments, dependent: :destroy
+    # belongs_to :user
+    # has_many :bookings, dependent: :destroy
+    # has_many :comments, dependent: :destroy
 
-    validates :adresse, presence: true
-    validates :start_date_of_availability, presence: true
-    validates :end_date_of_availability, presence: true
-    validates :voyageur, presence: true
-    validates :city, presence: true
-    validates :zipcode, presence: true
+    # validates :adresse, presence: true
+    # validates :start_date_of_availability, presence: true
+    # validates :end_date_of_availability, presence: true
+    # validates :voyageur, presence: true
+    # validates :city, presence: true
+    # validates :zipcode, presence: true
 
     ICONS = %i[cuisine couverts refrigirateur mini_refrigirateur four micro_onde lave_vaisselle congelateur lave_linge seche_linge baignoire savon_pour_le_corps shampooing fer seche_cheveux armoire cintre draps oreillers_couettes wifi work tv animaux clim fumeur no_smock]
     #
