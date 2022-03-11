0ec9ba61327d6f129032d3b46c88720917927456
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-11 17:08:25 +0100

    add type of row

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,22 +18,22 @@ csv_text = File.read(Rails.root.join('lib', 'seeds', 'logement.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'UTF-8')
 csv.each do |row|
     t = Logement.new
-    t.title = row['title']
-    t.adresse = row['adresse']
-    t.start_date_of_availability = row['start_date_of_availability']
-    t.end_date_of_availability = row['end_date_of_availability']
-    t.voyageur = row['voyageur']
-    t.created_at = row['created_at']
-    t.updated_at = row['updated_at']
-    t.city = row['city']
-    t.latitude = row['latitude']
-    t.longitude = row['longitude']
-    t.user_id = row['user_id']
-    t.images = row['images']
-    t.bookings_count = row['bookings_count']
-    t.comments_count = row['comments_count']
-    t.price = row['price']
-    t.description = row['description']
+    t.title = row['title'].string
+    t.adresse = row['adresse'].string
+    t.start_date_of_availability = row['start_date_of_availability'].date
+    t.end_date_of_availability = row['end_date_of_availability'].date
+    t.voyageur = row['voyageur'].integer
+    t.created_at = row['created_at'].datetime
+    t.updated_at = row['updated_at'].datetime
+    t.city = row['city'].string
+    t.latitude = row['latitude'].float
+    t.longitude = row['longitude'].float
+    t.user_id = row['user_id'].bigint
+    t.images = row['images'].string
+    t.bookings_count = row['bookings_count'].integer
+    t.comments_count = row['comments_count'].integer
+    t.price = row['price'].integer
+    t.description = row['description'].string
     t.equipments = row['equipments'].split(" ")
     t.save
     puts "#{t.title} saved"
