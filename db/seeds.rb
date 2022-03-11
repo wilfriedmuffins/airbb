0684a0f120b8ea818f1f5de7cda737ea2ed98344
wilfriedmuffins <wilfried.moffen@live.fr>
2022-03-11 11:26:52 +0100

    add logement with csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@
 
 require 'csv'
 
-csv_text = File.read(Rails.root.join('lib', 'seeds', 'logement_test.csv'))
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'logement.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
     t = Logement.new
